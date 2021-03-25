699cc77e9ce24299883b2f24b87b28ea67e8e90a
frapsky <jakubszostk@gmail.com>
2021-03-25 02:41:57 +0100

    Stage 3/6

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,11 @@
+package machine;
+
+
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+
+        System.out.println("Write how many ml of water the coffee machine has: ");
+
+    }
+}
